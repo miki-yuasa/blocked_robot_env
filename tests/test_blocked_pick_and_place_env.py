ab7354c004c14455517013070aacf39e9ac0da91
Mikihisa Yuasa <myuasa2@illinois.edu>
2024-11-04 21:55:50 -0600

    feat: add pick and place test

---
+++ tests/test_blocked_pick_and_place_env.py
@@ -0,0 +1,30 @@
+import os
+
+import imageio
+
+from blocked_robot_env.envs.fetch.pick_and_place import (
+    MujocoBlockedFetchPickAndPlaceEnv,
+)
+
+
+def test_env_init() -> None:
+    animation_save_path: str = "tests/out/plots/test_blocked_pick_and_place_env.gif"
+    env = MujocoBlockedFetchPickAndPlaceEnv(
+        render_mode="rgb_array",
+    )
+
+    obs, _ = env.reset()
+    frames = [env.render()]
+
+    for _ in range(50):
+        obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+        frames.append(env.render())
+
+        if terminated or truncated:
+            break
+        else:
+            pass
+
+    imageio.mimsave(animation_save_path, frames, fps=30)
+
+    assert os.path.exists(animation_save_path)
